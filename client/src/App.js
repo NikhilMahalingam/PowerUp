25cdfafdc1e35d7c07cf0c3cff92f9e6feceec7d
NikhilMahalingam <mahalingamnikhil@gmail.com>
2024-03-23 23:20:40 -0400

    Fixed setup, added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,31 @@
 import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import './App.css';
+import Home from './Home';
+import Goals from './Goals';
 
 function App() {
-  const [formData, setFormData] = useState({
-    age: '',
-    weight: '',
-    goals: '',
-    experience: ''
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await fetch('/api/submit', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    });
-    const data = await response.json();
-    alert(data.message);
-  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <form onSubmit={handleSubmit}>
-          <input type="number" name="age" placeholder="Age" value={formData.age} onChange={handleChange} /><br />
-          <input type="number" name="weight" placeholder="Weight" value={formData.weight} onChange={handleChange} /><br />
-          <input type="text" name="goals" placeholder="Goals" value={formData.goals} onChange={handleChange} /><br />
-          <input type="text" name="experience" placeholder="Experience" value={formData.experience} onChange={handleChange} /><br />
-          <button type="submit">Submit</button>
-        </form>
-      </header>
+      <Router>
+      <nav>
+          <Link to="/">Home</Link>
+          <Link to="/Goals">Goals</Link>
+      </nav>
+      <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/Goals" element={<Goals />} />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/Goals.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './Goals.css';
+
+function Goals() {
+
+    const [formData, setFormData] = useState({
+        age: '',
+        weight: '',
+        goals: '',
+        experience: ''
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+          ...prevState,
+          [name]: value
+        }));
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch('/api/submit', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
+        });
+        const data = await response.json();
+        alert(data.message);
+      };
+      
+  return (
+    <div>
+        <header className="App-header">
+                <form onSubmit={handleSubmit}>
+                <input type="number" name="age" placeholder="Age" value={formData.age} onChange={handleChange} /><br />
+                <input type="number" name="weight" placeholder="Weight" value={formData.weight} onChange={handleChange} /><br />
+                <input type="text" name="goals" placeholder="Goals" value={formData.goals} onChange={handleChange} /><br />
+                <input type="text" name="experience" placeholder="Experience" value={formData.experience} onChange={handleChange} /><br />
+                <button type="submit">Submit</button>
+        </form>
+        </header>
+    </div>
+  );
+}
+
+export default Goals;
